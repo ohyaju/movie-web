7ad6da3a171ce5703443888bd6e2435643ff8b56
ohyaju <bnomin257@gmail.com>
2025-03-11 12:27:58 +0800

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,49 @@
 'use client'
 
-import { useEffect } from "react";
-import { ACCESS_TOKEN } from "../..";
+import { ACCESS_TOKEN } from "./index";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+type MovieType = {
+  id: number;
+  title: string;
+  poster_path: string;
+};
 export default function Home() {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        `Bearer ${ACCESS_TOKEN}`,
-    },
-  };
+  const [movieList, setMovieList] = useState<MovieType[]>([]);
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     accept: "application/json",
+  //     Authorization:
+  //       `Bearer ${ACCESS_TOKEN}`,
+  //   },
+  // };
   const getMovies = async () => {
-    const res = await fetch(
-      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
-      options
+    const movies = await axios.get("https://api.themoviedb.org/3/discover/movie",
+      {
+        headers: {
+          Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+      }
     );
-    const movies = await res.json();
-    console.log(movies)
+    setMovieList(movies.data.results);
   };
-      
+
   useEffect(() => {
-getMovies();
+    getMovies();
   }, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 
+    pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {movieList.map((movie)=> {
+        return (
+          <div key={movie.id}>
+            <h1>{movie.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}/>
+            </div>
+        );
+      })}
 
     </div>
   );
