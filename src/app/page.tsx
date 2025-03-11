10bcfb01040e376eec9eb41f45509b0055444270
ohyaju <bnomin257@gmail.com>
2025-03-11 10:08:24 +0800

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
 'use client'
 
 import { useEffect } from "react";
+import { ACCESS_TOKEN } from "../..";
 
 export default function Home() {
-  useEffect (()=> {
-    const options = {
-
-    }
-  }
-  )
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        `Bearer ${ACCESS_TOKEN}`,
+    },
+  };
+  const getMovies = async () => {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+      options
+    );
+    const movies = await res.json();
+    console.log(movies)
+  };
+      
+  useEffect(() => {
+getMovies();
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+
     </div>
   );
 }
