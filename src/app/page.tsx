667fcd8835b1fef9da27b93626ad8bf0e76657f2
ohyaju <bnomin257@gmail.com>
2025-03-13 12:25:34 +0800

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,15 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 
 type MovieType = {
@@ -52,7 +61,7 @@ export default function Home() {
 
               <CarouselItem key={movie.id}>
                 <h1>{movie.title}</h1>
-                <img className="w-full" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
+                <img className="w-full" src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} />
               </CarouselItem>
             );
           })}
@@ -62,6 +71,13 @@ export default function Home() {
       </Carousel>
     </div>
     <h1>Upcoming</h1>
+    <Card>
+  <CardHeader>
+    <CardTitle></CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    {/* <p>Card Content</p> */}
     <div className="grid grid-flow-col grid-rows-4 gap-8">
       {movieList.map((movie)=> {
         return (
@@ -73,6 +89,12 @@ export default function Home() {
       })}
 
     </div>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+
     // </div>
   );
 }
